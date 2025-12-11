3bafeeb1fe2725055b5b72af549d052a757cb2b4
Garima Bisht <bishtgarima124@gmail.com>
2025-12-11 23:41:24 +0530

    api fixes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: counsellingapp.netlify.app, credentials: true }));
 app.use(express.json()); // allows JSON in body
 
 
