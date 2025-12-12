81dc8d213b56060eab96e420cfc559e35665ff67
Garima Bisht <bishtgarima124@gmail.com>
2025-12-12 07:58:28 +0530

    cors issue

--- server/server.js
+++ server/server.js
@@ -8,9 +8,11 @@ dotenv.config();
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors({
+  origin: "https://counsellingapp.netlify.app",
+  credentials: true
+}));
 
-app.use(cors({ origin: 'https://counsellingapp.netlify.app'
-, credentials: true }));
 app.use(express.json()); // allows JSON in body
 
 
@@ -47,3 +49,4 @@ app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
 });
 
+export default app;
