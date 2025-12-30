061d5787f4cc7fb09ea702a3be63a12d09e3dc47
Garima <118539728+Garimabisht12@users.noreply.github.com>
2025-12-30 07:08:26 +0000

    api call changes

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useStudentAuth } from "@/context/StudentAuth";
-import axios from "axios";
+import axios from "../../api/axios";
 import { ArrowLeft, Eye, EyeOff } from "lucide-react";
 
 export default function SignUp() {
