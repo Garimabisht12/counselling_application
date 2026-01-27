8d6f9e5f1a9fc0a125d7f07f53c5389860e157ff
Garima Bisht <bishtgarima124@gmail.com>
2026-01-27 22:28:10 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', process.env.CLIENT_URL], credentials: true }));
 app.use(express.json()); // allows JSON in body
 
 
