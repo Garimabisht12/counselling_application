daf9ebfe0a3c4d8af27935192696fd07307f564a
Garima Bisht <bishtgarima124@gmail.com>
2026-01-27 22:29:56 +0530

    deployment add client url

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: ['http://localhost:5173', process.env.CLIENT_URL], credentials: true }));
+app.use(cors({ origin: [process.env.CLIENT_URL, 'http://localhost:5173'], credentials: true }));
 app.use(express.json()); // allows JSON in body
 
 
